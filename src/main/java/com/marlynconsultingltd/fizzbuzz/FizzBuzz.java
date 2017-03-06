cb181c5f9a587009aa211f26936ed8854de730b2
Martin Warnett <admin@marlynconsultingltd.co.uk>
2017-03-06 14:53:02 +0000

    Amended main class to receive the parameters from validator.

--- src/main/java/com/marlynconsultingltd/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/marlynconsultingltd/fizzbuzz/FizzBuzz.java
@@ -7,14 +7,14 @@ import com.marlynconsultingltd.fizzbuzz.validation.ParameterValidator;
  * @author martinwarnett
  */
 public class FizzBuzz {
-    public void validateParameters(final ParameterValidator validator,
+    public Parameters validateParameters(final ParameterValidator validator,
                                    final String... args) {
-        validator.validateAndAssignParameters(args);
+        return validator.validateAndAssignParameters(args);
     }
 
     
     public static void main(String... args) {
         final FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.validateParameters(new ParameterValidator(), args);
+        final Parameters parameters = fizzBuzz.validateParameters(new ParameterValidator(), args);
     }
 }
